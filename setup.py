5679d2c19659e367f21bbd3faa689efb7bcd22eb
srcolinas <sebastian.rodriguez@landing.ai>
2020-08-04 14:06:45 -0500

    add circleci initial config

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-required = ["pytest", "coverage", "pytest-cov", "black"]
+required = ["pytest", "pytest-xdist", "pytest-cov", "black", "mypy"]
 
 setup(
     name="lac",
     version="0.0.1",
     description="A companion to study linear algebra",
-    url="https://github.com/srcolinas/lac",
+    url="https://github.com/open-workbooks/linear-algebra",
     python_requires=">=3.6",
     packages=find_packages(),
     include_package_data=True,
